f00d272c6167a41c8f80b991ba957ee27c90c548
Adam Thomas <adamthomas.atx@gmail.com>
2025-10-10 21:07:52 -0600

    Carousel fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./index-1.html", "./src/**/*.{html,js}", "./is-3700/**/*.{html,js}", "./p10-racing/**/*.{html,js}"],
+  content: ["./index.html", "./index-1.html", "./src/**/*.{html,js}", "./is-3700/**/*.{html,js}", "./p10-racing/**/*.{html,js}", "./case-studies/**/*.{html,js}"],
 }
